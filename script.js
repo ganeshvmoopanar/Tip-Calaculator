e1360d0b516691e8c170a6ad685a2b81da19ce57
Ganesh V M <144470644+ganeshvmoopanar@users.noreply.github.com>
2023-10-24 23:47:29 -0500

    image update

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function updateTipPercentage() {
     tipPercentageValue.textContent = `${percentage}%`; 
 }
 
-const backgrounds = ['1.avif', '2.avif', '3.avif', '4.avif', '5.avif', '6.avif', '7.avif', '8.avif', '9.avif', '10.avif', '11.avif', '12.avif', '13.avif', '14.avif', '15.avif', '16.avif', '17.avif', '18.avif', '19.avif', '20.avif'];
+const backgrounds = ['1.avif', '2.avif', '3.avif', '4.avif', '5.avif', '6.avif', '7.avif', '8.avif', '9.avif', '12.avif', '13.avif', '14.avif', '15.avif', '18.avif', '19.avif', '20.avif'];
 let currentBackgroundIndex = backgrounds.length - 1;
 function changeBackground() {
     if (currentBackgroundIndex < 0) {
